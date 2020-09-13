aecdd072de0e443a77f4ede31e956a3f689876de
Aniket2107 <had096705@gmail.com>
2020-09-13 18:00:14 +0530

    Fianl touch

--- frontend/src/components/elements/Favourites.js
+++ frontend/src/components/elements/Favourites.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-<<<<<<< HEAD
 import { Card, Modal, Button } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
@@ -28,24 +27,6 @@ export function Favouritemodal({ _id }) {
         style={{ color: "red" }}
         onClick={handleShow}
       />
-=======
-import { Card,Modal,Button } from "react-bootstrap";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from "@fortawesome/free-regular-svg-icons";
-import { faHeart as sHeart } from "@fortawesome/free-solid-svg-icons";
-import delUserFavourite from "../User/helper";
-
-export function Favouritemodal(){
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  return (
-    <>
-      
-      <FontAwesomeIcon icon={sHeart} style={{color:"red"}} onClick={handleShow} />
->>>>>>> bb7299436dd890e6973cac064b10e259437855a9
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
@@ -56,7 +37,6 @@ export function Favouritemodal(){
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-<<<<<<< HEAD
           <Button
             variant="danger"
             onClick={() => {
@@ -64,9 +44,6 @@ export function Favouritemodal(){
               removeFavourite(_id);
             }}
           >
-=======
-          <Button variant="danger" onClick={handleClose,delUserFavourite}>
->>>>>>> bb7299436dd890e6973cac064b10e259437855a9
             Remove
           </Button>
         </Modal.Footer>
@@ -75,7 +52,6 @@ export function Favouritemodal(){
   );
 }
 
-<<<<<<< HEAD
 function Favourites({ quote, author, background, color, _id }) {
   return (
     <Card style={{ width: "18rem" }}>
@@ -91,22 +67,3 @@ function Favourites({ quote, author, background, color, _id }) {
 }
 
 export default Favourites;
-=======
-
-
-function Favourites(){
-    return(
-        <Card style={{ width: '18rem' }}>
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-    <Card.Text>
-      <Favouritemodal />
-    </Card.Text>
-  </Card.Body>
-</Card>
-    );
-}
-
-export default Favourites;
->>>>>>> bb7299436dd890e6973cac064b10e259437855a9
--- frontend/src/components/elements/ProfileCard.js
+++ frontend/src/components/elements/ProfileCard.js
@@ -4,11 +4,7 @@ import Switch from "@material-ui/core/Switch";
 import { isAuthenticated } from "../Auth/helper";
 import { updateUser } from "../User/helper";
 
-<<<<<<< HEAD
 const { user, token } = isAuthenticated();
-=======
-const { user, token } = isAuthenticated;
->>>>>>> bb7299436dd890e6973cac064b10e259437855a9
 
 const editBtnStyle = {
   margin: "0",
@@ -21,7 +17,6 @@ function ProfileCard(props) {
   const [editStatus, setEditStatus] = useState(false);
 
   const [userBody, setUserBody] = useState({
-<<<<<<< HEAD
     username: isAuthenticated() ? isAuthenticated().user.username : "",
     isAdult: false,
     loading: false,
@@ -83,24 +78,6 @@ function ProfileCard(props) {
     );
   };
 
-=======
-    username: "",
-    password: "",
-    isAdult: false,
-    loading: false,
-  });
-
-  const onEdit = (event) => {
-    event.preventDefault();
-    setUserBody({ ...userBody, loading: true });
-    updateUser(props.user._id, token, userBody).then(() => {
-      setUserBody({
-        ...userBody,
-      });
-    });
-  };
-
->>>>>>> bb7299436dd890e6973cac064b10e259437855a9
   return (
     <div
       className="card"
@@ -124,36 +101,24 @@ function ProfileCard(props) {
           className="col-md-9 d-flex align-items-start flex-column"
           style={{ textAlign: "start", margin: " 20px 0", padding: "10px" }}
         >
-<<<<<<< HEAD
           {successMessage()}
           {errorMessage()}
           <div style={{ width: "80%", padding: "10px" }}>
             <h4>
               <span>Username: </span>
-=======
-          <div style={{ width: "80%", padding: "10px" }}>
-            <h4>
-              <span>Name:</span>
->>>>>>> bb7299436dd890e6973cac064b10e259437855a9
               <br />
               {editStatus ? (
                 <input
                   type="text"
                   className="form-control"
                   style={{
-<<<<<<< HEAD
                     width: "100%",
                     margin: 0,
-=======
-                    width: "100%", 
-                    margin: 0, 
->>>>>>> bb7299436dd890e6973cac064b10e259437855a9
                     fontSize: "20px",
                     border: "none",
                     borderBottom: "1px solid white",
                     background: "rgba(255,255,255,.1)",
                     color: "white",
-<<<<<<< HEAD
                     borderRadius: "0px",
                   }}
                   onChange={(e) => {
@@ -167,16 +132,6 @@ function ProfileCard(props) {
                 />
               ) : (
                 <>{username}</>
-=======
-                    borderRadius: "0px", }}
-                  onChange={(e) => {
-                    setUserBody({ ...userBody, username: e.target.value });
-                  }}
-                  value={userBody.username || props.user.username}
-                />
-              ) : (
-                <>{userBody.username || props.user.username}</>
->>>>>>> bb7299436dd890e6973cac064b10e259437855a9
               )}
             </h4>
           </div>
@@ -185,25 +140,17 @@ function ProfileCard(props) {
             <h4>
               <span>Email</span>
               <br />
-<<<<<<< HEAD
               {user.email}
-=======
-              {props.user.email}
->>>>>>> bb7299436dd890e6973cac064b10e259437855a9
             </h4>
           </div>
 
           <button
             className="btn btn-outline-info edit-user-btn"
             style={editBtnStyle}
-<<<<<<< HEAD
             onClick={() => {
               setEditStatus(!editStatus);
               setUserBody({ ...userBody, success: "", error: "", loading: "" });
             }}
-=======
-            onClick={() => setEditStatus(!editStatus)}
->>>>>>> bb7299436dd890e6973cac064b10e259437855a9
           >
             {editStatus ? "Cancel" : "Edit"}
           </button>
@@ -213,11 +160,7 @@ function ProfileCard(props) {
               <h4>
                 Are you an adult? &nbsp;
                 <Switch
-<<<<<<< HEAD
                   checked={isAdult}
-=======
-                  checked={userBody.isAdult || props.user.isAdult}
->>>>>>> bb7299436dd890e6973cac064b10e259437855a9
                   onChange={(e) => {
                     setUserBody({ ...userBody, isAdult: e.target.checked });
                   }}
